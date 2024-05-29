11dd4affe9c33d05d78e42b8e61db37f65929075
paigauresh <paigauresh@gmail.com>
2024-05-29 23:56:20 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import '../public/styles/Footer.css'; // Assuming the CSS file is located here
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} Gauresh. All rights reserved.</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
