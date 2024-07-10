22180f082768fde54d9d481eec43212d67b06c8f
paigauresh <paigauresh@gmail.com>
2024-07-10 13:11:01 +0530

    reset

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Main from '@/components/Main';
+import '@/public/styles/globals.css'
 
 const page = () => {
   return (
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,45 +1,9 @@
-import React from 'react';
-import Link from 'next/link';
+import React from 'react'
 
-const Header: React.FC = () => {
+const Header = () => {
   return (
-    <header className="bg-gray-900 text-white py-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" legacyBehavior>
-          <a className="text-2xl font-bold">MelodyStream</a>
-        </Link>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="/" legacyBehavior>
-                <a className="hover:text-gray-300">Home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/music" legacyBehavior>
-                <a className="hover:text-gray-300">Music</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/playlists" legacyBehavior>
-                <a className="hover:text-gray-300">Playlists</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/about" legacyBehavior>
-                <a className="hover:text-gray-300">About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact" legacyBehavior>
-                <a className="hover:text-gray-300">Contact</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
-};
+    <div>Header</div>
+  )
+}
 
-export default Header;
+export default Header
